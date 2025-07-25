fbe3393a675f7d75ef1be219c6e7d52b624645dc
raphael <raphaelcamachado@gmail.com>
2025-07-25 17:08:01 -0300

    Initial commit

---
+++ src/main/java/dev/raphael/fridgeai/FridgeAiApplication.java
@@ -0,0 +1,13 @@
+package dev.raphael.fridgeai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FridgeAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FridgeAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/raphael/fridgeai/FridgeAiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.raphael.fridgeai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FridgeAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
